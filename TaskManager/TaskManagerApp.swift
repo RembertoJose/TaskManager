5941c3082b60f19db4a3b9f03ee4cc38242dfcbc
Remberto Nunez <rembertojnunez@gmail.com>
2025-03-12 08:49:41 -0400

    Initial Commit

---
+++ TaskManager/TaskManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagerApp.swift
+//  TaskManager
+//
+//  Created by Remberto Nunez on 3/12/25.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
